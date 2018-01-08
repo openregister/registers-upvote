ca2202ae7b2fbf752c3b8a8a5f4ce34f2bc21255
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-08 17:45:40 +0000

    Add basic scaffolding for data

---
+++ app/controllers/registers_controller.rb
@@ -0,0 +1,46 @@
+class RegistersController < ApplicationController
+  before_action :set_register, only: [:edit, :update, :destroy]
+
+  def index
+    @registers = Register.order(:name)
+  end
+
+  def new
+    @register = Register.new
+  end
+
+  def edit
+  end
+
+  def create
+    @register = Register.new(register_params)
+    if @register.save
+      redirect_to registers_path, notice: 'Register was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @register.update(register_params)
+      redirect_to registers_path, notice: 'Register was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @register.destroy
+    redirect_to registers_path, notice: 'Register was successfully destroyed.'
+  end
+
+  private
+
+    def set_register
+      @register = Register.find(params[:id])
+    end
+
+    def register_params
+      params.require(:register).permit(:name)
+    end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def link_to_field_with_error(object, attribute)
+    # For "details" text areas we want the link to point to the
+    # container div so that the user can have some context regarding
+    # the error (the conatiner will contain the name heading) so we
+    # need to remove the "_details" part as the convention in the
+    # text toggle attributes(where this tends to be an issue) is to
+    # have an attribute and an accompanying attribute_details too.
+    field = attribute.to_s.gsub(/_details\Z/, '')
+
+    link_to(
+      object.errors.full_messages_for(attribute).first,
+      "##{object.class.name.underscore}_#{field}"
+    )
+  end
 end
