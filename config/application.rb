5d024c7b792ab44d21d389f93daced925e357feb
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-09 17:08:49 +0000

    Add CSP for govuk template inline scripts

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,22 @@ module RegistersUpvote
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Add recommended security headers and apply a basic lenient Content Security Policy
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'DENY',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff',
+      'Content-Security-Policy' => "default-src 'self'; " +
+        "font-src data:; " +
+        "img-src 'self'; " +
+        "object-src 'none'; " +
+        # the script digests are for the two inline scripts in govuk_template.gem:govuk_template.html.erb
+        # if the scripts in that file change, or more are added, use a command similar to
+        # this to generate the digests:
+        # `echo "'sha256-"$(echo -n "inline javascript text" | openssl dgst -sha256 -binary | openssl enc -base64)"'"`
+        "script-src 'self' 'unsafe-eval' 'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=' 'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g=' 'unsafe-inline'; " +
+        "style-src 'self' 'unsafe-inline'"
+    }
   end
 end
