98f4965971ead9935fb8662caf7bc9b0621a8302
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-10 15:28:06 +0000

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
+  root 'registers#index'
 
   get 'register', to: 'users#new', as: 'register'
   get 'signin', to: 'sessions#new', as: 'signin'
