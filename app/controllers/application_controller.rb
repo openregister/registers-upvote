adf9aba810f0c7a733d2e43a8a39ab02685c8d17
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-11 21:00:50 +0000

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if Rails.env.production?
+
   private
 
   def current_user
